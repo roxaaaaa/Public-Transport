1301a4d079a64094bf66cab9fbf6b09fc351741e
iwantsleepagain <hrystynakudryk@gmail.com>
2024-03-02 22:22:11 +0200

    trying to do smth

--- mainclass.h
+++ mainclass.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "iostream"
+#include "fstream"
+#include "string"
+using namespace std;
+
+class Transport {
+private:
+	string number;
+	int wheels;
+	int ageLimit;
+public:
+	Transport();
+	Transport(string num, int whe, int agel);
+	Transport(Transport& t);
+
+	string getNumber();
+	int getWheels();
+	int getAgeLim();
+
+	void inTransport(istream& i);
+	void printTransport(ostream& o);
+
+	friend istream& operator>>(istream& is, Transport& t);
+	friend ostream& operator<<(ostream& os, Transport& t);
+};
